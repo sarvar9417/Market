04cd65e1f302ea733516eac522ed2044fc993c52
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2022-05-03 08:24:04 +0500

    cdd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import { Market } from "./market/Market";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Market } from './market/Market'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
       <Market />
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
