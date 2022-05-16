c75614701bf7f7be01fb236739800ac1c2c89a1f
Umidjon8787 <100425651+Umidjon8787@users.noreply.github.com>
2022-05-16 18:21:19 +0500

    Sale-style

--- frontend/src/market/sections/director/sale/Sale.js
+++ frontend/src/market/sections/director/sale/Sale.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Products } from './Products'
+import { Selling } from './Selling'
 
 export const Sale = () => {
     return (
-        <div>
-            Sotuv bo'limi
+        <div className='container'>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-5'>
+                <div className='col-span-2 w-full'> <Products/></div>
+                <div className='col-span-3 w-full'><Selling/></div>    
+            </div>
         </div>
     )
 }
