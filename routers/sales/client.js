61c32caf1c54d2a47c57387de3f1251b0cb857f4
Navruz <navuz.ochilov98@mail.ru>
2022-05-20 16:07:01 +0300

    finished

--- routers/sales/client.js
+++ routers/sales/client.js
@@ -1,16 +1,17 @@
 const { Client, validateClient } = require("../../models/Sales/Client.js");
 const { Market } = require("../../models/MarketAndBranch/Market");
+const { Packman } = require("../../models/Sales/Packman.js");
 
 module.exports.register = async (req, res) => {
   try {
-    const { name, market } = req.body;
-    const { error } = validateClient(req.body);
+    const { name, market, packman } = req.body;
+    const { error } = validateClient({ name, market });
+
     if (error) {
       return res.status(400).json({
         error: error.message,
       });
     }
-
     const marke = await Market.findById(market);
     if (!marke) {
       return res.status(400).json({
@@ -33,9 +34,23 @@ module.exports.register = async (req, res) => {
       name,
       market,
     });
-
     await newClient.save();
-    res.status(201).send(newClient);
+    const checkpackman = await Packman.findById(packman);
+    if (checkpackman) {
+      newClient.packman = checkpackman._id;
+      await Packman.findByIdAndUpdate(checkpackman._id, {
+        $push: {
+          clients: newClient,
+        },
+      });
+      await newClient.save();
+    }
+
+    const sendingClient = await Client.findById(newClient._id)
+      .select("name")
+      .populate("packman", "name");
+
+    res.status(201).send(sendingClient);
   } catch (error) {
     res.status(400).json({ error: "Serverda xatolik yuz berdi..." });
   }
@@ -51,7 +66,9 @@ module.exports.getAll = async (req, res) => {
       });
     }
 
-    const client = await Client.find({ market });
+    const client = await Client.find({ market })
+      .select("name")
+      .populate("packman", "name");
 
     res.status(201).send(client);
   } catch (error) {
@@ -61,7 +78,7 @@ module.exports.getAll = async (req, res) => {
 
 module.exports.updateClient = async (req, res) => {
   try {
-    const { _id, market, name } = req.body;
+    const { _id, market, name, packman } = req.body;
     const marke = await Market.findById(market);
     if (!marke) {
       return res
@@ -75,10 +92,40 @@ module.exports.updateClient = async (req, res) => {
         .json({ message: `Diqqat! ${name} mijoz avval yaratilmagan` });
     }
 
-    await Client.findByIdAndUpdate(_id, {
+    const updatedClient = {
       name: name,
+    };
+    const packMan = await Packman.findById(packman);
+    if (packMan) {
+      await Packman.findByIdAndUpdate(client.packman._id, {
+        $pull: {
+          clients: client._id,
+        },
+      });
+      console.log(packMan);
+      if (client.packman._id !== packMan._id) {
+        await Packman.findByIdAndUpdate(packMan._id, {
+          $push: {
+            clients: { _id, name },
+          },
+        });
+      } else {
+        await Packman.findByIdAndUpdate(client.packman._id, {
+          $push: {
+            clients: { _id, name },
+          },
+        });
+      }
+      updatedClient.packman = { name: packMan.name, _id: packMan._id };
+    }
+
+    await Client.findByIdAndUpdate(_id, {
+      ...updatedClient,
     });
-    res.status(201).send(client);
+
+    const sendingClient = await Client.findById(_id);
+
+    res.status(201).send(sendingClient);
   } catch (error) {
     res.status(501).json({ error: "Serverda xatolik yuz berdi..." });
   }
@@ -86,7 +133,7 @@ module.exports.updateClient = async (req, res) => {
 
 module.exports.deleteClient = async (req, res) => {
   try {
-    const { _id, market, name } = req.body;
+    const { _id, market, name, packman } = req.body;
 
     const marke = await Market.findById(market);
     if (!marke) {
@@ -101,6 +148,15 @@ module.exports.deleteClient = async (req, res) => {
         .json({ message: `Diqqat! ${name} mijoz avval yaratilmagan!` });
     }
 
+    const packMan = await Packman.findById(packman);
+
+    if (packMan) {
+      await Packman.findByIdAndUpdate(packman, {
+        $pull: {
+          clients: client._id,
+        },
+      });
+    }
     await Client.findByIdAndDelete(_id);
 
     res.status(201).send(client);
