ffc0cb531ad83f402623b84426b841b579ccc2bd
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2022-06-03 14:34:04 +0500

    csdcs

--- frontend/src/market/sections/director/filials/Filials.js
+++ frontend/src/market/sections/director/filials/Filials.js
@@ -11,7 +11,7 @@ import React, { useCallback, useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 import { useHttp } from "../../../hooks/http.hook";
 import { FileUpload } from "../../../loginAndRegister/fileUpLoad/FileUpload";
-import { FilialTable } from "./filialComponenets/FilialTable";
+import { FilialTable } from "./filialComponenets/filialTable";
 import { Rows } from "./filialComponenets/Rows";
 
 const styleDefault = {
@@ -281,37 +281,36 @@ export const Filials = () => {
 
   return (
     <div>
-      <div className="m-3 px-2">
+      <div className='m-3 px-2'>
         <button
-          className="w-full bg-blue-800 font-bold text-white py-1 rounded"
-          onClick={() => setIsHide(!isHide)}
-        >
+          className='w-full bg-blue-800 font-bold text-white py-1 rounded'
+          onClick={() => setIsHide(!isHide)}>
           Tahrirlash
         </button>
         <div className={`${isHide ? "row g-3 " : "d-none"}`}>
-          <div className="col-md-6">
-            <div className="col-md-12">
+          <div className='col-md-6'>
+            <div className='col-md-12'>
               <FormControl isRequired>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   Filial nomi
                 </FormLabel>
                 <Input
                   onKeyUp={keyPressed}
-                  className="is-valid"
+                  className='is-valid'
                   placeholder={"Filial nomini kiriting"}
-                  size="sm"
+                  size='sm'
                   style={
                     branch.name && branch.name.length > 0
                       ? styleGreen
                       : styleDefault
                   }
                   onChange={changeHandler}
-                  name="name"
+                  name='name'
                   value={(branch.name && branch.name) || ""}
                 />
               </FormControl>
             </div>
-            <div className="col-md-12">
+            <div className='col-md-12'>
               <FormControl>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"Tashkilot nomi"}
@@ -319,19 +318,19 @@ export const Filials = () => {
                 <Input
                   onKeyUp={keyPressed}
                   placeholder={"Tashkilot nomini kiriting"}
-                  size="sm"
+                  size='sm'
                   style={
                     branch.organitionName && branch.organitionName.length > 0
                       ? styleGreen
                       : styleDefault
                   }
-                  name="organitionName"
+                  name='organitionName'
                   onChange={changeHandler}
                   value={(branch.organitionName && branch.organitionName) || ""}
                 />
               </FormControl>
             </div>
-            <div className="col-md-12">
+            <div className='col-md-12'>
               <FormControl>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"Manzil"}
@@ -339,19 +338,19 @@ export const Filials = () => {
                 <Input
                   onKeyUp={keyPressed}
                   placeholder={"Manzilni kiriting"}
-                  size="sm"
+                  size='sm'
                   style={
                     branch.address && branch.address.length > 0
                       ? styleGreen
                       : styleDefault
                   }
-                  name="address"
+                  name='address'
                   onChange={changeHandler}
                   value={(branch.address && branch.address) || ""}
                 />
               </FormControl>
             </div>
-            <div className="col-md-12">
+            <div className='col-md-12'>
               <FormControl>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"Mo'ljal"}
@@ -359,26 +358,26 @@ export const Filials = () => {
                 <Input
                   onKeyUp={keyPressed}
                   placeholder={"Mo'ljalni kiriting"}
-                  size="sm"
+                  size='sm'
                   style={
                     branch.orientation && branch.orientation.length > 0
                       ? styleGreen
                       : styleDefault
                   }
-                  name="orientation"
+                  name='orientation'
                   onChange={changeHandler}
                   value={(branch.orientation && branch.orientation) || ""}
                 />
               </FormControl>
             </div>
-            <div className="col-md-12">
+            <div className='col-md-12'>
               <FormControl isRequired>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"Telefon raqam1"}
                 </FormLabel>
                 <InputGroup>
                   <InputLeftAddon
-                    children="+998"
+                    children='+998'
                     style={
                       branch.phone1 && branch.phone1.length > 0
                         ? styleGreen
@@ -387,29 +386,29 @@ export const Filials = () => {
                   />
                   <Input
                     onKeyUp={keyPressed}
-                    type="tel"
+                    type='tel'
                     placeholder={"Telefon raqamni kiriting"}
-                    size="sm"
+                    size='sm'
                     style={
                       branch.phone1 && branch.phone1.length > 0
                         ? styleGreen
                         : styleDefault
                     }
-                    name="phone1"
+                    name='phone1'
                     onChange={changeHandler}
                     value={(branch.phone1 && branch.phone1) || ""}
                   />
                 </InputGroup>
               </FormControl>
             </div>
-            <div className="col-md-12">
+            <div className='col-md-12'>
               <FormControl>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"Telefon raqam2"}
                 </FormLabel>
                 <InputGroup>
                   <InputLeftAddon
-                    children="+998"
+                    children='+998'
                     style={
                       branch.phone2 && branch.phone2.length > 0
                         ? styleGreen
@@ -418,29 +417,29 @@ export const Filials = () => {
                   />
                   <Input
                     onKeyUp={keyPressed}
-                    type="tel"
+                    type='tel'
                     placeholder={"Telefon raqamni kiriting"}
-                    size="sm"
+                    size='sm'
                     style={
                       branch.phone2 && branch.phone2.length > 0
                         ? styleGreen
                         : styleDefault
                     }
-                    name="phone2"
+                    name='phone2'
                     onChange={changeHandler}
                     value={(branch.phone2 && branch.phone2) || ""}
                   />
                 </InputGroup>
               </FormControl>
             </div>
-            <div className="col-md-12">
+            <div className='col-md-12'>
               <FormControl>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"Telefon raqam3"}
                 </FormLabel>
                 <InputGroup>
                   <InputLeftAddon
-                    children="+998"
+                    children='+998'
                     style={
                       branch.phone3 && branch.phone3.length > 0
                         ? styleGreen
@@ -449,15 +448,15 @@ export const Filials = () => {
                   />
                   <Input
                     onKeyUp={keyPressed}
-                    type="tel"
+                    type='tel'
                     placeholder={"Telefon raqamni kiriting"}
-                    size="sm"
+                    size='sm'
                     style={
                       branch.phone3 && branch.phone3.length > 0
                         ? styleGreen
                         : styleDefault
                     }
-                    name="phone3"
+                    name='phone3'
                     onChange={changeHandler}
                     value={(branch.phone3 && branch.phone3) || ""}
                   />
@@ -465,8 +464,8 @@ export const Filials = () => {
               </FormControl>
             </div>
           </div>
-          <div className="col-md-6">
-            <div className="col-md-12">
+          <div className='col-md-6'>
+            <div className='col-md-12'>
               <FormControl>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"Bank nomi"}
@@ -474,55 +473,55 @@ export const Filials = () => {
                 <Input
                   onKeyUp={keyPressed}
                   placeholder={"Bank nomini kiriting"}
-                  size="sm"
+                  size='sm'
                   style={
                     branch.bank && branch.bank.length > 0
                       ? styleGreen
                       : styleDefault
                   }
-                  name="bank"
+                  name='bank'
                   onChange={changeHandler}
                   value={(branch.bank && branch.bank) || ""}
                 />
               </FormControl>
             </div>
-            <div className="col-md-12">
+            <div className='col-md-12'>
               <FormControl>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"INN"}
                 </FormLabel>
                 <Input
                   onKeyUp={keyPressed}
-                  type="number"
+                  type='number'
                   placeholder={"INN ni kiriting"}
-                  size="sm"
+                  size='sm'
                   style={
                     branch.inn && branch.inn.length > 0
                       ? styleGreen
                       : styleDefault
                   }
-                  name="inn"
+                  name='inn'
                   onChange={changeHandler}
                   value={(branch.inn && branch.inn) || 0}
                 />
               </FormControl>
             </div>
-            <div className="col-md-12">
+            <div className='col-md-12'>
               <FormControl>
                 <FormLabel style={{ color: "#38B2AC", marginTop: "1rem" }}>
                   {"Hisob raqam"}
                 </FormLabel>
                 <Input
                   onKeyUp={keyPressed}
-                  type="number"
+                  type='number'
                   placeholder={"Hisob raqamni kiriting"}
-                  size="sm"
+                  size='sm'
                   style={
                     branch.bankNumber && branch.bankNumber.length > 0
                       ? styleGreen
                       : styleDefault
                   }
-                  name="bankNumber"
+                  name='bankNumber'
                   onChange={changeHandler}
                   value={(branch.bankNumber && branch.bankNumber) || 0}
                 />
@@ -543,22 +542,21 @@ export const Filials = () => {
             </FormControl>
           </div>
 
-          <div className="col-md-6 text-center mt-4">
+          <div className='col-md-6 text-center mt-4'>
             {loading || load ? (
-              <Button isLoading colorScheme="teal" variant="solid"></Button>
+              <Button isLoading colorScheme='teal' variant='solid'></Button>
             ) : (
               <Button
-                colorScheme="teal"
-                variant="solid"
-                onClick={updateHandler}
-              >
+                colorScheme='teal'
+                variant='solid'
+                onClick={updateHandler}>
                 {"Tahrirlash"}
               </Button>
             )}
           </div>
         </div>
       </div>
-      <div className="m-3">
+      <div className='m-3'>
         <FilialTable
           setPageSize={setPageSize}
           branchInputChange={searchName}
