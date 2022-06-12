c5ad3cb7439fd791c36519ddced1e986a8ceedbc
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2022-06-12 18:16:44 +0500

    sdcs

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const { start } = require('./connectDB/db')
-start(app)
+const { start } = require('./connectDB/db');
+start(app);
 
-const { routers } = require('./routers/routers')
-routers(app)
+const { routers } = require('./routers/routers');
+routers(app);
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'frontend', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
