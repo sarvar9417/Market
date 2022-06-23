2d10c3a9274be2a1448a0cd636df7603f136e110
Navruz <navuz.ochilov98@mail.ru>
2022-06-23 13:44:55 +0300

    sale debug

--- frontend/src/market/sections/director/sale/Sales/ChequeConnectors/Header.js
+++ frontend/src/market/sections/director/sale/Sales/ChequeConnectors/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 export const Header = ({ auth, sales }) => {
   return (
-    <div className='grid grid-cols-3 items-center'>
-      <div className='flex flex-col'>
+    <div className='grid grid-cols-6 px-4 gap-x-4 items-center'>
+      <div className='col-span-2 flex flex-col'>
         <p className='flex justify-between'>
           <span className='font-bold'>{t("Do'kon")}:</span>{' '}
           <span>{auth.market.name}</span>
@@ -26,10 +26,22 @@ export const Header = ({ auth, sales }) => {
           <span>{sales.client && sales.client[0] && sales.client[0].name}</span>
         </p>
       </div>
-      <div className='text-center text-xl font-bold'>
-        {t('Sotuv')}: A{sales.id}
+      <div className='px-4 col-span-4 flex flex-col '>
+        <div className='flex justify-between items-center mb-2'>
+          <div className='text-center text-xl font-bold'>
+            {t('Sotuv')}: A{sales.id}
+          </div>
+          <div className='text-right text-2xl font-bold uppercase'>
+            {auth.market.name}
+          </div>
+        </div>
+        <div className='flex justify-between items-center'>
+          <div className='text-center text-xl font-bold'>Sotuvchi:</div>
+          <div className='text-right text-2xl font-bold'>
+            {auth.user.firstname} {auth.user.lastname}
+          </div>
+        </div>
       </div>
-      <div className='text-right text-2xl font-bold '>PIPE HOUSE</div>
     </div>
   );
 };
