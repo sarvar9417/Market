9c5626968d42b0cea5d3000b374cad003be6aa9b
Navruz <navuz.ochilov98@mail.ru>
2022-06-27 11:09:03 +0300

    frfr

--- frontend/src/market/sections/director/inventory/Print.js
+++ frontend/src/market/sections/director/inventory/Print.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useReactToPrint } from 'react-to-print';
 import { Header } from './Print/Header';
 import { Table } from './Print/Table';
@@ -29,12 +29,33 @@ export const Print = ({ inventories, inventoriesConnector, setCheck }) => {
   });
 
   const auth = useContext(AuthContext);
+
+  const [plusInventories, setPlusInventories] = useState([]);
+  const [minusInventories, setMinusInventories] = useState([]);
+
+  useEffect(() => {
+    let minusI = [];
+    let plusI = inventories.filter((inv) => {
+      inv.inventorycount - inv.productcount < 0 && minusI.push(inv);
+      return inv.inventorycount - inv.productcount > 0;
+    });
+    setPlusInventories(
+      plusI.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+    );
+    setMinusInventories(
+      minusI.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+    );
+  }, [inventories]);
+
   return (
     <div className='absolute top-0 right-0 z-50 w-full min-h-screen bg-white overflow-auto font-mono'>
       <div className='a4 m-auto w-[27cm]' ref={componentRef}>
         <Header auth={auth} inventoriesConnector={inventoriesConnector} />
         <hr />
-        <Table inventories={inventories} />
+        <Table
+          inventories={plusInventories}
+          minusInventories={minusInventories}
+        />
       </div>
       <div className='py-4 w-full'>
         <Control setCheck={setCheck} print={print} />
