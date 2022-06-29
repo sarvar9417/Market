84860f235788a6007534b0ce9c05e42146be8aab
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2022-06-29 13:37:06 +0500

    add

--- frontend/src/market/sections/director/DirectorRouter.js
+++ frontend/src/market/sections/director/DirectorRouter.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { EditDirector } from './editDirector/EditDirector';
 import { EditDirectorPassword } from './editDirector/EditDirectorPassword';
 import { Category } from './products/Categorys';
-// import { ProductType } from './products/ProductType';
 import { Unit } from './products/Unit';
 import { Exchangerate } from './products/Exchangerate';
 import { Product } from './products/Product';
 import { Incoming } from './products/Incoming';
 import { Supplier } from './products/Supplier';
-// import { Brand } from './products/Brand';
 import { Sale } from './sale/Sale';
 import { Packman } from './sale/Packman';
 import { Client } from './sale/Client';
@@ -24,8 +22,53 @@ import { Inventories } from './inventory/Inventories';
 import { HomePage } from './homepage/HomePage';
 import { Report } from './cashRegister/Report';
 import { Sellers } from './sale/Sellers';
+import { useHttp } from '../../hooks/http.hook';
+import { AuthContext } from '../../context/AuthContext';
 
 export const DirectorRouter = () => {
+  const { request } = useHttp();
+  const auth = useContext(AuthContext);
+
+  const [permission, setPermission] = useState({
+    creates: false,
+    incomings: false,
+    inventories: false,
+    sales: false,
+    reporters: false,
+    sellers: false,
+    calculations: false,
+    branches: false,
+    mainmarket: false,
+  });
+
+  const getPermission = useCallback(async () => {
+    try {
+      const data = await request(
+        `/api/administrator/getpermission`,
+        'POST',
+        {
+          permissionid: auth.market && auth.market.permission,
+        },
+        {
+          Authorization: `Bearer ${auth.token}`,
+        }
+      );
+      if (data) {
+        setPermission(data);
+      }
+    } catch (error) {
+      // notify({
+      //   title: error,
+      //   description: '',
+      //   status: 'error',
+      // });
+    }
+  }, [request, auth]);
+
+  useEffect(() => {
+    getPermission();
+  }, [getPermission]);
+
   return (
     <div>
       <Switch>
@@ -38,39 +81,49 @@ export const DirectorRouter = () => {
         <Route path='/alo24/editdirectorpassword'>
           <EditDirectorPassword />
         </Route>
-        <Route path='/alo24/category'>
-          <Category />
-        </Route>
-        {/* <Route path='/alo24/producttypes'>
-          <ProductType />
-        </Route> */}
-        <Route path='/alo24/product'>
-          <Product />
-        </Route>
-        <Route path='/alo24/unit'>
-          <Unit />
-        </Route>
-        <Route path='/alo24/supplier'>
-          <Supplier />
-        </Route>
-        {/* <Route path='/alo24/brand'>
-          <Brand />
-        </Route> */}
+        {permission.creates && (
+          <Route path='/alo24/category'>
+            <Category />
+          </Route>
+        )}
+        {permission.creates && (
+          <Route path='/alo24/product'>
+            <Product />
+          </Route>
+        )}
+        {permission.creates && (
+          <Route path='/alo24/unit'>
+            <Unit />
+          </Route>
+        )}
+        {permission.creates && (
+          <Route path='/alo24/supplier'>
+            <Supplier />
+          </Route>
+        )}
         <Route path='/alo24/exchangerate'>
           <Exchangerate />
         </Route>
-        <Route path='/alo24/incoming'>
-          <Incoming />
-        </Route>
-        <Route path='/alo24/branchregister'>
-          <FilialRegister />
-        </Route>
-        <Route path='/alo24/branches'>
-          <Filials />
-        </Route>
-        <Route path='/alo24/branchdirector'>
-          <FilialDirector />
-        </Route>
+        {permission.incomings && (
+          <Route path='/alo24/incoming'>
+            <Incoming />
+          </Route>
+        )}
+        {permission.branches && (
+          <Route path='/alo24/branchregister'>
+            <FilialRegister />
+          </Route>
+        )}
+        {permission.branches && (
+          <Route path='/alo24/branches'>
+            <Filials />
+          </Route>
+        )}
+        {permission.branches && (
+          <Route path='/alo24/branchdirector'>
+            <FilialDirector />
+          </Route>
+        )}
         <Route path='/alo24/packman'>
           <Packman />
         </Route>
@@ -80,27 +133,41 @@ export const DirectorRouter = () => {
         <Route path='/alo24/reports'>
           <Report />
         </Route>
-        <Route path='/alo24/sales'>
-          <Sale />
-        </Route>
-        <Route path='/alo24/discounts'>
-          <Discounts />
-        </Route>
-        <Route path='/alo24/payments'>
-          <Payments />
-        </Route>
-        <Route path='/alo24/debts'>
-          <Debts />
-        </Route>
-        <Route path='/alo24/inventory'>
-          <Inventory />
-        </Route>
-        <Route path='/alo24/inventories'>
-          <Inventories />
-        </Route>
-        <Route path='/alo24/sellers'>
-          <Sellers />
-        </Route>
+        {permission.sales && (
+          <Route path='/alo24/sales'>
+            <Sale />
+          </Route>
+        )}
+        {permission.calculations && (
+          <Route path='/alo24/discounts'>
+            <Discounts />
+          </Route>
+        )}
+        {permission.calculations && (
+          <Route path='/alo24/payments'>
+            <Payments />
+          </Route>
+        )}
+        {permission.calculations && (
+          <Route path='/alo24/debts'>
+            <Debts />
+          </Route>
+        )}
+        {permission.inventories && (
+          <Route path='/alo24/inventory'>
+            <Inventory />
+          </Route>
+        )}
+        {permission.inventories && (
+          <Route path='/alo24/inventories'>
+            <Inventories />
+          </Route>
+        )}
+        {permission.sellers && (
+          <Route path='/alo24/sellers'>
+            <Sellers />
+          </Route>
+        )}
 
         <Redirect to='/alo24' />
       </Switch>
